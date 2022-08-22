f9e182b0914adf9ae7a840c1aeb9d469bad739ee
daingo <daingo7211@gmail.com>
2022-08-22 11:16:55 +0700

    done growth

--- src/ChartLayout.tsx
+++ src/ChartLayout.tsx
@@ -1,67 +1,21 @@
 import React from "react";
 import styled from "styled-components";
 import { _a } from "./Growth";
+
 type Props = {
   children: React.ReactNode;
   numberOfChart: number;
   renderLabel: () => JSX.Element[];
+  strokeWidth: number;
+  width: number | undefined;
 };
 function ChartLayout({
   children,
   numberOfChart = 1,
   renderLabel,
-}: Partial<Props>) {
-  const _calcXCircle = (
-    _index: number,
-    _group: number,
-    _numberOfElementBefore: number
-  ) => {
-    const _startXPointOfCircle = 70;
-    const _distanceXPoint = 30;
-    const _distanceXGroup = 60;
-
-    const _totalDistanceXPoint =
-      _numberOfElementBefore * _distanceXPoint - _distanceXPoint * _group;
-
-    const result =
-      _startXPointOfCircle +
-      _group * _distanceXGroup +
-      _index * _distanceXPoint +
-      _totalDistanceXPoint;
-
-    return result;
-  };
-
-  console.log(
-    "%c=> :::: _calcX ::::",
-    "color:orange;font-weight:bold;",
-    _calcXCircle(1, 2, 10)
-  );
-  const _radian = 10;
-  const _fraction = 60 / 20;
-  const _distanceYCentralPoint = 20;
-  const _startYCircle = 320;
-  /**
-   * 20% => 60px
-   *
-   * 320px => y start point of circle : 0%
-   *
-   * 310px => y start point of rect : 0%
-   *
-   * xCircle = _calcXCircle(.....)
-   * yCircle = _startYCircle - toPercentageScore * _fraction
-   *
-   * xRect = xCircle - _radian;
-   * yRect = yCircle + heightLine
-   *
-   * heightLine = _percent * _fraction - _distanceYCentralPoint
-   *
-   * x1Line = xCircle
-   * y1Line = yCircle + 10 * Math.sign(growthPercentage) // negative
-   *
-   * x2Line = xCircle
-   * y2Line = y1Line + heightLine * Math.sign(growthPercentage) // positive
-   */
+  strokeWidth = 5,
+  width,
+}: Props) {
   return (
     <div className={"chart-container"}>
       <div className={"chart-left"}>
@@ -78,121 +32,16 @@ function ChartLayout({
         <ChartData>
           <ChartDataTop>
             <ChartWrapper>
-              <Svg width={numberOfChart * 140}>
+              <Svg width={width}>
                 <Line y={60 - 40} />
                 <Line y={120 - 40} />
                 <Line y={180 - 40} />
                 <Line y={240 - 40} />
                 <Line y={300 - 40} />
                 <LineBottom y={360 - 40} strokeWidth={2} />
-                {/*{children}*/}
-                <line
-                  x1="40"
-                  y1="20"
-                  x2="40"
-                  y2="320"
-                  stroke="#808285"
-                  strokeWidth="2"
-                />
-                {/*========================*/}
-                {_a.map(
-                  (
-                    { domainScores }: { domainScores: any[] },
-                    group: number
-                  ) => {
-                    return domainScores.map((item: any, index: number) => {
-                      let _numberOfEleBefore: number = 0;
-                      for (let i = 0; i < group; i++) {
-                        _numberOfEleBefore += _a[i].domainScores.length;
-                      }
-                      const xCircle = _calcXCircle(
-                        index,
-                        group,
-                        _numberOfEleBefore
-                      );
-                      const yCircle =
-                        _startYCircle - item.toPercentageScore * _fraction;
-
-                      const heightLine =
-                        item.percentageScore * _fraction -
-                        _distanceYCentralPoint;
-
-                      const xRect = xCircle - _radian;
-                      const yRect = yCircle + heightLine;
-
-                      const x1Line = xCircle;
-                      const y1Line =
-                        yCircle + 10 * Math.sign(item.growthPercentage); // negative
-
-                      const x2Line = xCircle;
-                      const y2Line =
-                        y1Line + heightLine * Math.sign(item.growthPercentage); // positive
-
-                      return (
-                        <React.Fragment key={index}>
-                          <rect
-                            x={xRect}
-                            y={yRect}
-                            width={20}
-                            height={20}
-                            style={{
-                              stroke: "#808285",
-                              strokeWidth: 1.25,
-                              fill: "#c88eb7",
-                            }}
-                          />
-                          <line
-                            x1={x1Line}
-                            y1={y1Line}
-                            x2={x2Line}
-                            y2={y2Line}
-                            stroke="#808285"
-                            strokeWidth="1"
-                          />
-                          <circle
-                            cx={xCircle}
-                            cy={yCircle}
-                            r="10"
-                            stroke="#808285"
-                            strokeWidth="1"
-                            fill="#c88eb7"
-                          />
-                        </React.Fragment>
-                      );
-                    });
-                  }
-                )}
-                <rect
-                  x={60}
-                  y={300}
-                  width={20}
-                  height={20}
-                  style={{
-                    stroke: "#808285",
-                    strokeWidth: 1.25,
-                    fill: "#c88eb7",
-                  }}
-                />
-                <line
-                  x1="70"
-                  y1="20"
-                  x2="70"
-                  y2="300"
-                  stroke="#808285"
-                  strokeWidth="1"
-                />
-                <circle
-                  cx="70"
-                  cy="10"
-                  r="10"
-                  stroke="#808285"
-                  strokeWidth="1"
-                  fill="#c88eb7"
-                />
-                {/*========================*/}
+                {children}
               </Svg>
-              {/*<ReportLabel index={0}>Lorem ipsum dolor sit amet.</ReportLabel>*/}
-              {/*{renderLabel()}*/}
+              {renderLabel()}
             </ChartWrapper>
           </ChartDataTop>
         </ChartData>
@@ -203,7 +52,7 @@ function ChartLayout({
 
 export default ChartLayout;
 
-const Svg = styled.svg<{ width: number }>`
+const Svg = styled.svg<{ width: number | undefined }>`
   min-width: 100%;
   width: ${(prop) => prop.width}px;
   height: 360px;
@@ -258,7 +107,3 @@ const ChartWrapper = styled.div`
   position: relative;
   width: 100%;
 `;
-
-const Rect = styled.rect<{ index: number; group: number }>``;
-const Circle = styled.circle<{ index: number; group: number }>``;
-const LineConnect = styled.line<{ index: number; group: number }>``;
--- src/Growth.tsx
+++ src/Growth.tsx
@@ -302,9 +302,410 @@ export const _a = [
       },
     ],
   },
+  {
+    studentId: 593353,
+    displayName: "FA12 FA12",
+    firstName: "FA12",
+    lastName: "FA12",
+    proficiencyLevel: "Partially Proficient",
+    score: 61,
+    fillColor: "#F5C839",
+    strokeColor: "#F5C839",
+    toScore: 88,
+    toProficiencyLevel: "Proficient",
+    toStrokeColor: "#88CA22",
+    domainScores: [
+      {
+        domainId: 39,
+        displayOrder: null,
+        domainName: "Key Ideas and Details: Literary Texts",
+        domainCode: "RL_KID",
+        domainColorCode: "#998675",
+        percentageScore: 80, // react
+        proficiencyLevel: "Proficient",
+        fillColor: "#998675",
+        strokeColor: "#808285",
+        growthPercentage: -10, //
+        toPercentageScore: 70, // circle
+        toProficiencyLevel: "Partially Proficient",
+        toFillColor: "#998675",
+        toStrokeColor: "#808285",
+        limitDomain: true,
+      },
+      {
+        domainId: 40,
+        displayOrder: null,
+        domainName: "Craft and Structure: Literary Texts",
+        domainCode: "RL_CS",
+        domainColorCode: "#E4412B",
+        percentageScore: 40,
+        proficiencyLevel: "Below Proficient",
+        fillColor: "#E4412B",
+        strokeColor: "#808285",
+        growthPercentage: 60,
+        toPercentageScore: 100,
+        toProficiencyLevel: "Proficient",
+        toFillColor: "#E4412B",
+        toStrokeColor: "#808285",
+        limitDomain: true,
+      },
+      {
+        domainId: 42,
+        displayOrder: null,
+        domainName: "Key Ideas and Details: Informational Texts",
+        domainCode: "RI_KID",
+        domainColorCode: "#F3CE69",
+        percentageScore: 88,
+        proficiencyLevel: "Proficient",
+        fillColor: "#F3CE69",
+        strokeColor: "#808285",
+        growthPercentage: -11,
+        toPercentageScore: 77,
+        toProficiencyLevel: "Partially Proficient",
+        toFillColor: "#F3CE69",
+        toStrokeColor: "#808285",
+        limitDomain: true,
+      },
+      {
+        domainId: 43,
+        displayOrder: null,
+        domainName: "Craft and Structure: Informational Texts",
+        domainCode: "RI_CS",
+        domainColorCode: "#C88EB7",
+        percentageScore: 42,
+        proficiencyLevel: "Below Proficient",
+        fillColor: "#C88EB7",
+        strokeColor: "#808285",
+        growthPercentage: 58,
+        toPercentageScore: 100,
+        toProficiencyLevel: "Proficient",
+        toFillColor: "#C88EB7",
+        toStrokeColor: "#808285",
+        limitDomain: true,
+      },
+      {
+        domainId: 44,
+        displayOrder: null,
+        domainName: "Integration of Knowledge and Ideas: Informational Texts",
+        domainCode: "RI_IKI",
+        domainColorCode: "#00B9B4",
+        percentageScore: 57,
+        proficiencyLevel: "Partially Proficient",
+        fillColor: "#00B9B4",
+        strokeColor: "#808285",
+        growthPercentage: 43,
+        toPercentageScore: 100,
+        toProficiencyLevel: "Proficient",
+        toFillColor: "#00B9B4",
+        toStrokeColor: "#808285",
+        limitDomain: true,
+      },
+      {
+        domainId: 44,
+        displayOrder: null,
+        domainName: "Integration of Knowledge and Ideas: Informational Texts",
+        domainCode: "RI_IKI",
+        domainColorCode: "#00B9B4",
+        percentageScore: 57,
+        proficiencyLevel: "Partially Proficient",
+        fillColor: "#00B9B4",
+        strokeColor: "#808285",
+        growthPercentage: 43,
+        toPercentageScore: 100,
+        toProficiencyLevel: "Proficient",
+        toFillColor: "#00B9B4",
+        toStrokeColor: "#808285",
+        limitDomain: true,
+      },
+    ],
+  },
+  {
+    studentId: 593353,
+    displayName: "FA12 FA12",
+    firstName: "FA12",
+    lastName: "FA12",
+    proficiencyLevel: "Partially Proficient",
+    score: 61,
+    fillColor: "#F5C839",
+    strokeColor: "#F5C839",
+    toScore: 88,
+    toProficiencyLevel: "Proficient",
+    toStrokeColor: "#88CA22",
+    domainScores: [
+      {
+        domainId: 39,
+        displayOrder: null,
+        domainName: "Key Ideas and Details: Literary Texts",
+        domainCode: "RL_KID",
+        domainColorCode: "#998675",
+        percentageScore: 80, // react
+        proficiencyLevel: "Proficient",
+        fillColor: "#998675",
+        strokeColor: "#808285",
+        growthPercentage: -10, //
+        toPercentageScore: 70, // circle
+        toProficiencyLevel: "Partially Proficient",
+        toFillColor: "#998675",
+        toStrokeColor: "#808285",
+        limitDomain: true,
+      },
+      {
+        domainId: 40,
+        displayOrder: null,
+        domainName: "Craft and Structure: Literary Texts",
+        domainCode: "RL_CS",
+        domainColorCode: "#E4412B",
+        percentageScore: 40,
+        proficiencyLevel: "Below Proficient",
+        fillColor: "#E4412B",
+        strokeColor: "#808285",
+        growthPercentage: 60,
+        toPercentageScore: 100,
+        toProficiencyLevel: "Proficient",
+        toFillColor: "#E4412B",
+        toStrokeColor: "#808285",
+        limitDomain: true,
+      },
+      {
+        domainId: 42,
+        displayOrder: null,
+        domainName: "Key Ideas and Details: Informational Texts",
+        domainCode: "RI_KID",
+        domainColorCode: "#F3CE69",
+        percentageScore: 88,
+        proficiencyLevel: "Proficient",
+        fillColor: "#F3CE69",
+        strokeColor: "#808285",
+        growthPercentage: -11,
+        toPercentageScore: 77,
+        toProficiencyLevel: "Partially Proficient",
+        toFillColor: "#F3CE69",
+        toStrokeColor: "#808285",
+        limitDomain: true,
+      },
+      {
+        domainId: 43,
+        displayOrder: null,
+        domainName: "Craft and Structure: Informational Texts",
+        domainCode: "RI_CS",
+        domainColorCode: "#C88EB7",
+        percentageScore: 42,
+        proficiencyLevel: "Below Proficient",
+        fillColor: "#C88EB7",
+        strokeColor: "#808285",
+        growthPercentage: 58,
+        toPercentageScore: 100,
+        toProficiencyLevel: "Proficient",
+        toFillColor: "#C88EB7",
+        toStrokeColor: "#808285",
+        limitDomain: true,
+      },
+      {
+        domainId: 44,
+        displayOrder: null,
+        domainName: "Integration of Knowledge and Ideas: Informational Texts",
+        domainCode: "RI_IKI",
+        domainColorCode: "#00B9B4",
+        percentageScore: 57,
+        proficiencyLevel: "Partially Proficient",
+        fillColor: "#00B9B4",
+        strokeColor: "#808285",
+        growthPercentage: 43,
+        toPercentageScore: 100,
+        toProficiencyLevel: "Proficient",
+        toFillColor: "#00B9B4",
+        toStrokeColor: "#808285",
+        limitDomain: true,
+      },
+      {
+        domainId: 44,
+        displayOrder: null,
+        domainName: "Integration of Knowledge and Ideas: Informational Texts",
+        domainCode: "RI_IKI",
+        domainColorCode: "#00B9B4",
+        percentageScore: 57,
+        proficiencyLevel: "Partially Proficient",
+        fillColor: "#00B9B4",
+        strokeColor: "#808285",
+        growthPercentage: 43,
+        toPercentageScore: 100,
+        toProficiencyLevel: "Proficient",
+        toFillColor: "#00B9B4",
+        toStrokeColor: "#808285",
+        limitDomain: true,
+      },
+    ],
+  },
 ];
 function Growth() {
-  return <ChartLayout />;
+  /**
+   * 20% => 60px
+   *
+   * 320px => y start point of circle : 0%
+   *
+   * 310px => y start point of rect : 0%
+   *
+   * xCircle = _calcXCircle(.....)
+   * yCircle = _startYCircle - toPercentageScore * _fraction
+   *
+   * xRect = xCircle - _radian;
+   * yRect = yCircle + heightLine
+   *
+   * heightLine = _percent * _fraction - _distanceYCentralPoint
+   *
+   * x1Line = xCircle
+   * y1Line = yCircle + 10 * Math.sign(growthPercentage) // negative
+   *
+   * x2Line = xCircle
+   * y2Line = y1Line + heightLine * Math.sign(growthPercentage) // positive
+   */
+  const calcXCircle = (
+    _index: number,
+    _group: number,
+    _numberOfElementBefore: number
+  ) => {
+    const START_X_POINT_CIRCLE = 70;
+    const DISTANCE_X_POINT = 30;
+    const DISTANCE_X_GROUP = 60;
+
+    const _totalDistanceXPoint =
+      _numberOfElementBefore * DISTANCE_X_POINT - DISTANCE_X_POINT * _group;
+
+    return (
+      START_X_POINT_CIRCLE +
+      _group * DISTANCE_X_GROUP +
+      _index * DISTANCE_X_POINT +
+      _totalDistanceXPoint
+    );
+  };
+
+  const convertPercentageToPx = (percentage: number) => {
+    // 60px = 20%
+    const PX = 60;
+    const PERCENTAGE = 20;
+    const RATIO = PX / PERCENTAGE;
+
+    return percentage * RATIO;
+  };
+
+  const calcYCircle = (_startYPoint: number, _percentage: number) => {
+    return _startYPoint - convertPercentageToPx(_percentage);
+  };
+
+  const calcNumberOfElementBefore = (_a: any[], _group: number) => {
+    let _numberOfEleBefore: number = 0;
+    for (let i = 0; i < _group; i++) {
+      _numberOfEleBefore += _a[i].domainScores.length;
+    }
+
+    return _numberOfEleBefore;
+  };
+  const calcWidthSvg = (data: any[]) => {
+    const numberOfElementBefore = calcNumberOfElementBefore(data, data.length);
+    return calcXCircle(0, data.length, numberOfElementBefore);
+  };
+  const RADIAN = 10;
+  const START_Y_CIRCLE = 320;
+  const START_Y_RECT = 310;
+  return (
+    <ChartLayout
+      renderLabel={() => {
+        return _a.map((item, index) => {
+          return (
+            <ReportLabel index={index}>
+              {item.firstName} {item.lastName}
+            </ReportLabel>
+          );
+        });
+      }}
+      numberOfChart={_a.length}
+      strokeWidth={2}
+      width={calcWidthSvg(_a)}
+    >
+      {_a.map((_, index) => {
+        const numberOfElementBefore = calcNumberOfElementBefore(_a, index + 1);
+        const DISTANCE_X_CIRCLE_AND_LINEBREAK = 30;
+        const x =
+          calcXCircle(0, index + 1, numberOfElementBefore) -
+          DISTANCE_X_CIRCLE_AND_LINEBREAK;
+
+        return (
+          <line
+            key={index}
+            x1={x}
+            y1="20"
+            x2={x}
+            y2="320"
+            stroke="#808285"
+            strokeWidth="2"
+          />
+        );
+      })}
+      {/*========================*/}
+      {_a.map(({ domainScores }: { domainScores: any[] }, group: number) => {
+        return domainScores.map((item: any, index: number) => {
+          const numberOfElementBefore = calcNumberOfElementBefore(_a, group);
+
+          const xCircle = calcXCircle(index, group, numberOfElementBefore);
+          const yCircle = calcYCircle(START_Y_CIRCLE, item.toPercentageScore);
+
+          const heightLine = convertPercentageToPx(
+            Math.abs(item.growthPercentage)
+          );
+
+          const xRect = xCircle - RADIAN;
+          const yRect = calcYCircle(START_Y_RECT, item.percentageScore);
+
+          const x1Line = xCircle;
+          const x2Line = xCircle;
+
+          const y1Line = yRect;
+          const y2Line = y1Line - heightLine * Math.sign(item.growthPercentage);
+
+          return (
+            <React.Fragment key={index}>
+              <line
+                x1={x1Line}
+                y1={y1Line}
+                x2={x2Line}
+                y2={y2Line}
+                stroke="#808285"
+                strokeWidth="1"
+              />
+              <rect
+                x={xRect}
+                y={yRect}
+                width={20}
+                height={20}
+                stroke={item.strokeColor}
+                strokeWidth="1.25"
+                fill={item.fillColor}
+              />
+              <circle
+                cx={xCircle}
+                cy={yCircle}
+                r="10"
+                stroke={item.toStrokeColor}
+                strokeWidth="1"
+                fill={item.toFillColor}
+              />
+            </React.Fragment>
+          );
+        });
+      })}
+    </ChartLayout>
+  );
 }
 
 export default Growth;
+const ReportLabel = styled.div<{ index: number }>`
+  position: absolute;
+  left: ${(props) => props.index * 180 + 130}px;
+  top: 345px;
+  font-size: 14.666666666px;
+  transform: translate(-75%, 0%) rotate(-45deg);
+  transform-origin: top right;
+  width: 65px;
+  text-align: center;
+  display: inline-block;
+`;
